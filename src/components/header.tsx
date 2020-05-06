424084ef756e4fb9e4771e001885994f96074152
James Best <james.best@candide.eu>
2020-05-06 18:49:40 +0100

    Added header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,42 +1,66 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled, { css } from "../utils/styled-components"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const Header = () => (
+  <HeaderWrapper>
+    <header>
+      <div className="header-left">
+        <Logo>
+          <Link to="/">JB</Link>
+        </Logo>
+        <LogoWrapper />
+      </div>
+      <div></div>
+    </header>
+  </HeaderWrapper>
 )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+const HeaderWrapper = styled.div`
+  ${() => css`
+    height: 8rem;
+    width: 100vw;
+    padding-top: 25px;
+    header {
+      width: 100vw;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      .header-left {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+    }
+  `}
+`
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+const LogoWrapper = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    width: 60px;
+    height: 60px;
+    border: 5px solid ${theme.colors.primary};
+    transition: transform 1.2s ease-in-out;
+    &:hover {
+      transform: rotate(360deg);
+    }
+  `}
+`
 
+const Logo = styled.span`
+  ${({ theme }) => css`
+    position: absolute;
+    a {
+      text-decoration: none;
+      font-size: 2.5rem;
+      color: ${theme.fonts.colors.primary};
+      font-weight: bold;
+      font-family: "Space Mono", monospace;
+      line-height: 40px;
+      letter-spacing: 0.1px;
+    }
+  `}
+`
 export default Header
