001ef5a6699176396bbb2b7b5e44d68846539c40
James Best <james.best@candide.eu>
2020-05-07 11:07:04 +0100

    Added me

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `James Best | Software Engineer | Hacker | Maker`,
+    description: `A Bristol based software engineer.`,
+    author: `@jimgbest`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `James Best`,
+        short_name: `James Best`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#45f9e5`,
+        theme_color: `#45f9e5`,
         display: `minimal-ui`,
-        icon: ``, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
