a7a78423aa3a48d406e3385e32b83f6b08a8c1c7
James Best <jim@justjibba.net>
2021-02-21 20:35:41 +0000

    Remove hacking from menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -35,15 +35,6 @@ const Header = () => {
               </li>
               <li>
                 <Link
-                  to="/hacking/"
-                  activeStyle={{ background: "#ff286c" }}
-                  partiallyActive={true}
-                >
-                  HACKING
-                </Link>
-              </li>
-              <li>
-                <Link
                   to="/uses/"
                   activeStyle={{ background: "#ff286c" }}
                   partiallyActive={true}
